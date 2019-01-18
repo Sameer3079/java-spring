a21005949a03f0cfdfa156e1b9fd7d6688a6f7e3
Sameer3079 <smrbasil4@gmail.com>
2019-01-18 11:37:42 +0530

    Initial Commit

---
+++ src/main/java/com/example/easynotes/domain/note/NotesRepository.java
@@ -0,0 +1,9 @@
+package com.example.easynotes.domain.note;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotesRepository extends JpaRepository<Note, Long>{
+
+}
---
+++ src/main/java/com/example/easynotes/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.example.easynotes.exception;
+
+public class ResourceNotFoundException {
+
+}
